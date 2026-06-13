7122292c10b0a0a82a69dacfc5b15da01db139a2
Adam Matthiesen <amatthiesen@outlook.com>
2026-06-13 15:22:03 -0700

    Add custom configuration for remapJar task; set archive base name and version

--- gradle/build-logic/src/main/kotlin/matthiesen.minecraft-module-conventions.gradle.kts
+++ gradle/build-logic/src/main/kotlin/matthiesen.minecraft-module-conventions.gradle.kts
@@ -1,4 +1,5 @@
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
+import net.fabricmc.loom.task.RemapJarTask
 import net.fabricmc.loom.task.RemapSourcesJarTask
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.named
@@ -11,6 +12,11 @@ pluginManager.withPlugin("dev.architectury.loom") {
     configure<LoomGradleExtensionAPI> {
         enableTransitiveAccessWideners.set(true)
         silentMojangMappingsLicense()
+    }
+
+    tasks.named<RemapJarTask>("remapJar") {
+        archiveBaseName.set("${rootProject.property("archives_base_name")}-${project.name}")
+        archiveVersion.set(project.version.toString())
     }
 
     tasks.named<RemapSourcesJarTask>("remapSourcesJar") {
